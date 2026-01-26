2a87c9e5a8de0fca7b9cb230a1304aa013f12959
Ben Schembri <benschembri@gmail.com>
2026-01-26 23:02:42 +1100

    claude: remove old codebase

--- src/components/helpers/moneyFormatter.ts
+++
@@ -1,8 +0,0 @@
-export default function moneyFormatter(number: number) {
-  const money = new Intl.NumberFormat('en-AU', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-
-  return money.format(number);
-}
